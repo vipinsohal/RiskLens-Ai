5271cd1105ee45962220777cec777bb29d93bfe4
vipinsohal <strom4614@gmail.com>
2026-04-27 13:25:00 +0530

    updated files

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-require('dotenv').config({ path: __dirname + '/.env' });
+require('dotenv').config();
 
 const connectDB = require('./config/db');
 
